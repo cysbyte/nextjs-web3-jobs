e281f23a2d182e1df7d7469ff812605e8d7692a5
chenyunshu2 <chenyunshu2@gmail.com>
2024-07-11 23:21:07 +0800

    fixed: versioning untracked files

--- src/components/layout/wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from "react";
-const Wrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="max-w-screen-xl mx-auto px-10 md:px-5">{children}</div>;
-};
-
-export default Wrapper;
